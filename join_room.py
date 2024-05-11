093af7d34a794504cf75e3895eedafd153a903ff
amine <108814041+amxnese@users.noreply.github.com>
2024-05-11 23:51:17 +0100

    modified 'join_room.py' file

--- join_room.py
+++ join_room.py
@@ -1,7 +1,14 @@
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import uic
+from room import Room
 
 class Join_Room(QMainWindow):
   def __init__(self):
     super(Join_Room, self).__init__()
     uic.loadUi("join_room.ui", self)
+    self.JoinButton.clicked.connect(self.join)
+    
+  def join(self):
+    self.close()
+    self.room_ui = Room()
+    self.room_ui.show()
