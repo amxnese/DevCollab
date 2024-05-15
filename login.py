133005c6455055f6f306e2a21c9e3b37726dd45f
amine <108814041+amxnese@users.noreply.github.com>
2024-05-15 17:27:54 +0100

    modified login.py file

--- login.py
+++ login.py
@@ -1,7 +1,11 @@
 from PyQt5.QtWidgets import QMainWindow
+from abstraction import Submitions
 from PyQt5 import uic
 
-class LoginUI(QMainWindow):
+class LoginUI(QMainWindow, Submitions):
   def __init__(self):
     super(LoginUI, self).__init__()
     uic.loadUi("login.ui", self)
+  
+  def submit(self):
+    pass
