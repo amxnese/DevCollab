45d5e73104bfe5be84ffe338fb186daeb407dff6
amine <108814041+amxnese@users.noreply.github.com>
2024-05-15 17:28:26 +0100

    modified 'register.py' file

--- register.py
+++ register.py
@@ -1,7 +1,11 @@
 from PyQt5.QtWidgets import QMainWindow
+from abstraction import Submitions
 from PyQt5 import uic
 
-class Register(QMainWindow):
+class Register(QMainWindow, Submitions):
   def __init__(self):
     super(Register, self).__init__()
     uic.loadUi("register.ui", self)
+
+  def submit(self):
+    pass
