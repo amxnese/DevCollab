de2cace54ca4b142d713013abb4b8c6533832ac1
amine <108814041+amxnese@users.noreply.github.com>
2024-05-20 11:39:24 +0100

    fixed some bugs in the home page and added some logic related to the user being logged in or not

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QSizePolicy
 from PyQt5 import uic
 import sys
 import os
 from login import LoginUI
-from register import Register
 from join_room import Join_Room
 from create_room import Create_Room
+import globals
 
 class MainUI(QMainWindow):
   def __init__(self):
@@ -15,37 +15,56 @@ class MainUI(QMainWindow):
     self.join_button.clicked.connect(self.join)
     self.login_button.clicked.connect(self.login)
     self.register_button.clicked.connect(self.register)
+    # checking if user is logged in
+    if globals.current_user:
+      self.welcome.setText(f'Welcome {globals.current_user}')
     self.show()
 
   def join(self):
-    # closing the current window
-    self.close()
-    # opening the login window
-    self.join_room = Join_Room()
-    self.join_room.show()
+    if not globals.current_user:
+      QMessageBox.warning(self, 'Warning', 'You have be logged in in order to join rooms')
+    else:
+      # closing the current window
+      self.close()
+      # opening the login window
+      self.join_room = Join_Room()
+      self.join_room.show()
 
   def create(self):
-    # closing the current window
-    self.close()
-    # opening the login window
-    self.create_room = Create_Room()
-    self.create_room.show()
+    # checking if logged in
+    if not globals.current_user:
+      QMessageBox.warning(self, 'Warning', 'You have be logged in in order to create rooms')
+    else:
+      # closing the current window
+      self.close()
+      # opening the login window
+      self.create_room = Create_Room()
+      self.create_room.show()
 
   def login(self):
-    # closing the current window
-    self.close()
-    # opening the login window
-    self.login_ui = LoginUI()
-    self.login_ui.show()
+    # checking if user already logged in
+    if globals.current_user:
+      QMessageBox.warning(self, 'Warning', f'You are already logged in as {globals.current_user}')
+    else:
+      # closing the current window
+      self.close()
+      # opening the login window
+      self.login_ui = LoginUI()
+      self.login_ui.show()
 
   def register(self):
-    # closing the current window
-    self.close()
-    # opening the login window
-    self.register_ui = Register()
-    self.register_ui.show()
+    if globals.current_user:
+      QMessageBox.warning(self, 'Warning', f'You are already logged in as {globals.current_user}')
+    else:
+      from register import Register
+      # closing the current window
+      self.close()
+      # opening the login window
+      self.register_ui = Register()
+      self.register_ui.show()
 
 # initialize the app
-app = QApplication(sys.argv)
-UIWindow = MainUI()
-app.exec_()
+if __name__ == '__main__':
+  app = QApplication(sys.argv)
+  UIWindow = MainUI()
+  app.exec_()
