f9d48b01e72b25aa3285ec4c4d17cc0291315228
amine <108814041+amxnese@users.noreply.github.com>
2024-05-27 22:37:47 +0100

    implemented the logic for joining rooms

--- join_room.py
+++ join_room.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QMainWindow, QMessageBox
 from PyQt5 import uic
 from room import Room
+import sqlite3
+import globals
 
 class Join_Room(QMainWindow):
   def __init__(self):
@@ -9,9 +11,21 @@ class Join_Room(QMainWindow):
     self.JoinButton.clicked.connect(self.join)
     
   def join(self):
-    if not self.room_id.text():
+    code = self.room_id.text()
+    if not code:
       QMessageBox.warning(self, 'Warning', "Please enter the room's id")
       return
-    self.close()
-    self.room_ui = Room()
-    self.room_ui.show()
+    conn = sqlite3.connect('myDb.db')
+    cur = conn.cursor()
+    query = cur.execute(f"select project_id from Projects where project_id='{code}'")
+    project_id = query.fetchone()
+    if not project_id:
+      QMessageBox.warning(self, 'Warning', "Room doesn't exist")
+      # clear the input
+      self.room_id.clear()
+    else:
+      project_id = project_id[0]
+      globals.current_project = project_id
+      self.close()
+      self.room_ui = Room()
+      self.room_ui.show()
