75973312b672b1ff382070d9da7fa9ea39926147
amine <108814041+amxnese@users.noreply.github.com>
2024-05-27 22:34:47 +0100

    handled the logic for creating new rooms and interactions with database

--- create_room.py
+++ create_room.py
@@ -1,5 +1,8 @@
 from PyQt5.QtWidgets import QMainWindow, QMessageBox
+from room import Room
 from PyQt5 import uic
+import sqlite3
+import globals
 
 class Create_Room(QMainWindow):
   def __init__(self):
@@ -8,5 +11,23 @@ class Create_Room(QMainWindow):
     self.CreateRoomButton.clicked.connect(self.submit)
     
   def submit(self):
-    if not self.room_name.text():
+    # getting the name of the project
+    name = self.room_name.text()
+    # checking if the name is valid
+    if not name:
       QMessageBox.warning(self, 'Warning', 'Please enter the name of the room')
+    else:
+      # inserting the project to the database
+      conn = sqlite3.connect("myDb.db")
+      cur = conn.cursor()
+      cur.execute('insert into Projects (username, name) values (?,?)',(globals.current_user, name))
+      # getting the id of the last inserted project
+      project_id =  cur.lastrowid
+      conn.commit()
+      conn.close()
+      globals.message = ['information', f"Your project's id is {project_id}"]
+      globals.current_project = project_id
+      self.close()
+      self.room_ui = Room()
+      self.room_ui.show()
+
