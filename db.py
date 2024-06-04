72c93685c8e3b1d625234a16aa0ce5497efad10b
amine <108814041+amxnese@users.noreply.github.com>
2024-06-04 23:39:14 +0100

    added the ui files and some others

---
+++ db.py
@@ -0,0 +1,8 @@
+# import sqlite3
+# import globals
+# conn = sqlite3.connect('myDb.db')
+# cur = conn.cursor()
+# likes = cur.execute(f'select likes from tasks where task_id=300').fetchone()
+# print(likes)
+# conn.close()
+# print('hi')
